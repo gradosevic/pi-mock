033bfbd572c2c0eaa09131ee5b49682f9dc54a6b
Goran Radosevic <gogyinsomnia@gmail.com>
2021-07-01 20:45:38 +0200

    Refactoring Pi Mock for simpler use and added a few additional configuration properties: - Added "debug" configuration property: to show logs in console - Added "production_domain" configuration property: to disable Pi Mock in production

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Pi Mock
 
-This library can be used for the development purpose ONLY of the Pi Network Apps. It mocks Pi methods and returns dummy
-data, so you can focus on the development and enable the real Pi library later.
+This library can be used for the development purpose ONLY of the Pi Network Apps. It mocks Pi SDK methods on front-end and returns dummy
+data, so you can focus on the front-end development and enable the real Pi library later.
+
 
 ## Installation
 
@@ -24,12 +25,22 @@ like this:
 <script src="/pi-mock.js"></script>
 ```
 
-## Configuring
+## Initialization
 
-After including Pi Mock script, you can configure further the mockin behavior. This is the full Mock Configuring object:
+The minimum required initialization for Pi Mock is:
 
 ```javascript
-   window.PiMockConfig = {
+    PiMock();
+```
+
+## Configuring
+
+After including Pi Mock script, you can configure further the mocking behavior or pass the configuration directly into PiMock when initializing.
+This is the full example with all configuration set:
+
+```javascript
+   PiMock({
+    production_domain: false,
     username: 'john_doe',
     uid: '12345678-1234-414e-b578-42e89d1f3c02',
     payment_found: {
@@ -39,23 +50,50 @@ After including Pi Mock script, you can configure further the mockin behavior. T
     },
     payment_error: false,
     payment_cancelled: false,
-}
+    debug: true
+});
 ```
 
-By default, this object is empty and will call success event handlers.
+By default, PiMock will call success event handlers.
 - "onReadyForServerApproval" and
 - "onReadyForServerCompletion", 2 seconds later
 
 ### Parameters
 
-- **username**: (Optional). If set, it will return the provided username
-- **uid**: (Optional). If set it will always return the provided uid, if not, it will always generate new uid.
-- **payment_found**: (Optional). Set this option with the expected payment object if you want to trigger "onOpenPaymentFound" event
-- **payment_error** (Optional). If set to "true" it will trigger error event on payment failure
-- **payment_cancelled** (Optional). If set to "true" it will trigger payment cancelled event
+- **production_domain**: (Optional|Default:false). Production domain name or subdomain (e.g. "example.com" or "subdomain.example.com"). If set, it will disable Pi Mock in production and use real Pi SDK instead, so you don't need to change your code with Pi Mock for production
+- **username**: (Optional|Default:"john_doe"). If set, it will return the provided username
+- **uid**: (Optional|Default:false). If set it will always return the provided uid, if not, it will always generate new uid
+- **payment_found**: (Optional|Default:false). Set this option with the expected payment object if you want to trigger "onOpenPaymentFound" event
+- **payment_error** (Optional|Default:false). If set to "true" it will trigger error event on payment failure
+- **payment_cancelled** (Optional|Default:false). If set to "true" it will trigger payment cancelled event
+- **debug** (Optional|Default:false). If set to "true" it will print out useful information in dev console
 
 ## Known issues
 Please note that expected data returned in event handlers might not be the same as expected in Pi library. Feel free to test and contribute so we can improve this.
 
 ## Tip
-You can re-configure "PiMockConfig" also later in your code, so you can call and test multiple payment calls with different data and different expected events in sequence.
+You can re-configure "PiMock()" also later in your code, passing different configuration so you can call and test multiple payment calls with different data and different expected events in sequence.
+
+### Example
+
+```javascript
+
+    //Make Pi SDK returns mocked successful results
+    PiMock();
+
+    //.. your code to test successful transactions
+
+    //Make Pi SDK returns errors
+    PiMock({payment_error: true});
+
+    //.. your code to test failed transactions
+
+    //Make Pi SDK returns successful results again and shows debug messages in console
+    PiMock({
+        payment_error: false,
+        debug: true
+    });
+
+
+
+```
--- pi-mock.js
+++ pi-mock.js
@@ -1,71 +1,141 @@
-//UUID Generator
-if (typeof window.uuidv4 === 'undefined') {
-    window.uuidv4 = function () {
+/**
+ * PiMock
+ *
+ * Mock Pi SDK library functions for development
+ */
+(function () {
+
+    var uuidv4 = function () {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     }
-}
 
-//Initialize Pi Mock configuration
-if (!window.PiMockConfig) {
-    window.PiMockConfig = {};
-}
+    var _pm = {
 
-Pi.authenticate = function (scopes, onOpenPaymentFound) {
-    if (PiMockConfig.payment_found) {
-        window.setTimeout(function () {
-            onOpenPaymentFound({
-                "identifier": (uuidv4().replaceAll('-', '')).substring(4),
-                "user_uid": PiMockConfig.uid || uuidv4(),
-                "amount": PiMockConfig.payment_found.amount,
-                "memo": PiMockConfig.payment_found.memo,
-                "metadata": PiMockConfig.payment_found.metadata,
-                "to_address": (uuidv4().replaceAll('-', '') + uuidv4().replaceAll('-', '')).substring(8).toUpperCase(),
-                "status": {
-                    "developer_approved": false,
-                    "transaction_verified": false,
-                    "developer_completed": false,
-                    "cancelled": false,
-                    "user_cancelled": false
-                },
-                "transaction": null,
-                "created_at": (new Date()).toISOString()
-            });
-        }, 100);
+        initialized: false,
+
+        //Initial Pi Mock configuration
+        configuration: {
+            production_domain: false,
+            username: 'john_doe',
+            uid: false,
+            payment_found: false,
+            payment_error: false,
+            payment_cancelled: false,
+            debug: false
+        },
+
+        //Disable Pi Mock on production domain
+        isProductionDomain: function () {
+            return _pm.configuration.production_domain && window.location.hostname === _pm.configuration.production_domain;
+        },
+
+        debug: function (message, data = null) {
+            if (this.configuration.debug) {
+                if(data) {
+                    console.log('PiMock: ' + message, data);
+                }else {
+                    console.log('PiMock: ' + message);
+                }
+            }
+        },
+
+        /**
+         * Mocks Pi SDK "authenticate" method
+         */
+        mockAuthenticate: function () {
+            window.Pi.authenticate = function (scopes, onOpenPaymentFound) {
+                _pm.debug('Calling mock "Pi.authenticate"...');
+                if (_pm.configuration.payment_found) {
+                    _pm.debug('Payment already exists');
+                    window.setTimeout(function () {
+                        _pm.debug('Calling "onOpenPaymentFound"...');
+                        onOpenPaymentFound({
+                            "identifier": (uuidv4().replaceAll('-', '')).substring(4),
+                            "user_uid": _pm.configuration.uid || uuidv4(),
+                            "amount": _pm.configuration.payment_found.amount,
+                            "memo": _pm.configuration.payment_found.memo,
+                            "metadata": _pm.configuration.payment_found.metadata,
+                            "to_address": (uuidv4().replaceAll('-', '') + uuidv4().replaceAll('-', '')).substring(8).toUpperCase(),
+                            "status": {
+                                "developer_approved": false,
+                                "transaction_verified": false,
+                                "developer_completed": false,
+                                "cancelled": false,
+                                "user_cancelled": false
+                            },
+                            "transaction": null,
+                            "created_at": (new Date()).toISOString()
+                        });
+                    }, 100);
+                }
+                return new Promise((resolve, reject) => {
+                    const response = {
+                        "user":
+                            {
+                                "username": _pm.configuration.username || 'john_doe',
+                                "uid": _pm.configuration.uid || uuidv4(),
+                                "roles": ["email_verified"]
+                            }, "accessToken": "1234567-1234567890AX31-1234567890aN10zfuoCE"
+                    };
+                    _pm.debug('"Pi.authenticate" returned response', response);
+                    resolve(response);
+                });
+            }
+        },
+
+        /**
+         * Mocks Pi SDK "createPayment" method
+         */
+        mockCreatePayment: function () {
+            window.Pi.createPayment = function (data, handlers) {
+                _pm.debug('Calling mock "Pi.createPayment"...');
+                var paymentId = uuidv4();
+                var txnId = uuidv4();
+                if (_pm.configuration.payment_error && handlers.onError) {
+                    //TODO: Set correct expected data for onError event handler
+                    _pm.debug('"Pi.createPayment:onError" handler called');
+                    handlers.onError('PiMock: Mocked error occurred', {
+                        "identifier": paymentId
+                    });
+                } else if (_pm.configuration.payment_cancelled && handlers.onCancel) {
+                    //TODO: Set correct expected data for onCancel event handler
+                    _pm.debug('"Pi.createPayment:onCancel(' + paymentId + ')" handler called');
+                    handlers.onCancel(paymentId);
+                } else if (handlers.onReadyForServerApproval && handlers.onReadyForServerCompletion) {
+                    //TODO: Set correct expected data for onReadyForServerApproval event handler
+                    _pm.debug('"Pi.createPayment:onReadyForServerApproval(' + paymentId + ')" handler called');
+                    handlers.onReadyForServerApproval(paymentId);
+                    window.setTimeout(function () {
+                        //TODO: Set correct expected data for onReadyForServerCompletion event handler
+                        _pm.debug('"Pi.createPayment:onReadyForServerCompletion(' + paymentId + ', ' + txnId + ')" handler called');
+                        handlers.onReadyForServerCompletion(paymentId, txnId);
+                    }, 2000);
+                } else {
+                    alert('PiMock: Please define event handlers in "createPayment" method');
+                }
+            }
+        },
+        mockPiMethods: function () {
+            this.mockAuthenticate();
+            this.mockCreatePayment();
+        }
+    };
+
+    window.PiMock = function (config) {
+        Object.assign(_pm.configuration, config);
+        if(!_pm.isProductionDomain() && !_pm.initialized){
+            if (_pm.configuration.production_domain) {
+                _pm.debug('Current domain is: ' +  window.location.hostname);
+            }
+            _pm.debug('Mocking Pi SDK methods...');
+            _pm.mockPiMethods();
+            _pm.initialized = true;
+            _pm.debug('Configuration', _pm.configuration);
+            _pm.debug('Pi SDK methods mocked successfully!');
+        }
     }
-    return new Promise((resolve, reject) => {
-        resolve({
-            "user":
-                {
-                    "username": PiMockConfig.username || 'john_doe',
-                    "uid": PiMockConfig.uid || uuidv4(),
-                    "roles": ["email_verified"]
-                }, "accessToken": "1234567-1234567890AX31-1234567890aN10zfuoCE"
-        });
-    });
-}
 
-Pi.createPayment = function (data, handlers) {
-    if (PiMockConfig.payment_error && handlers.onError) {
-        //TODO: Set correct expected data for onError event handler
-        handlers.onError('PiMock: Mocked error occurred', {
-            "identifier": (uuidv4())
-        });
-    } else if (PiMockConfig.payment_cancelled && handlers.onCancel) {
-        //TODO: Set correct expected data for onCancel event handler
-        handlers.onCancel(uuidv4());
-    } else if (handlers.onReadyForServerApproval && handlers.onReadyForServerCompletion) {
-        var paymentId = uuidv4();
-        var txnId = uuidv4();
-        //TODO: Set correct expected data for onReadyForServerApproval event handler
-        handlers.onReadyForServerApproval(paymentId);
-        window.setTimeout(function () {
-            //TODO: Set correct expected data for onReadyForServerCompletion event handler
-            handlers.onReadyForServerCompletion(paymentId, txnId);
-        }, 2000);
-    } else {
-        alert('PiMock: Please define event handlers in "createPayment" method');
-    }
-}
+})();
